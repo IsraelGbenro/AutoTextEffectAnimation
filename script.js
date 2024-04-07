f8c3c47ae86736409902dbc1ea24b2a924218b0e
Israelgbenro <akannigbenro@gmail.com>
2024-04-07 20:06:51 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const containerEl = document.querySelector(".container");
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+const careers = ["Youtuber", "Web Developer", "Frelancer", "Instructor"];
+
+updateText()
+
+function updateText(){
+characterIndex++;
+containerEl.innerHTML = `<h1>I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(0, characterIndex)}</h1>`;
+
+if(characterIndex === careers[careerIndex].length){
+ careerIndex++
+ characterIndex = 0
+}
+if(careerIndex === careers.length){
+careerIndex = 0
+}
+setTimeout(updateText, 400);
+};
